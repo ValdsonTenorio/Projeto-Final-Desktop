d342d9bc4690d48f86598d730277a91ea11693ca
Valdson <valdsonmartinstenorio721@gmail.com>
2024-05-07 18:51:41 -0400

    adicão arquivo persistence

--- ProjetoDesktop/src/main/java/br/unigran/persistence/Dados.java
+++ ProjetoDesktop/src/main/java/br/unigran/persistence/Dados.java
@@ -4,10 +4,41 @@
  */
 package br.unigran.persistence;
 
+import br.unigran.entities.Funcionario;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author laboratorio
  */
 public class Dados {
-    
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("br.unigran_Sistema_jar_1.0-SNAPSHOTPU");
+    EntityManager em = emf.createEntityManager();
+    EntityTransaction etx = em.getTransaction();
+
+    public void salvar(Object o){
+        etx.begin();
+        em.persist(o);
+        etx.commit();
+    }
+    public void atualiza(Object o){
+        etx.begin();
+        em.merge(o);
+        etx.commit();     
+    }    
+    public void remove(Object o){
+        etx.begin();
+        em.remove(o);
+        etx.commit(); 
+    }
+    public List listar(){
+        return em.createNativeQuery("select * from funcionario f",Funcionario.class).getResultList();
+    }
+    public List listaOO(){
+        return em.createNativeQuery("select * from funcionario f").getResultList();
+    }
 }
