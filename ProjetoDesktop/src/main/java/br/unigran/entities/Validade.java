3a08af18338056ac6830adc91f2447a1df3b4eff
Valdson Martins Tenorio <valdsonmartinstenorio721@gmail.com>
2024-05-07 13:53:14 -0400

    Salvando alteracoes entidade validade

--- ProjetoDesktop/src/main/java/br/unigran/entities/Validade.java
+++ ProjetoDesktop/src/main/java/br/unigran/entities/Validade.java
@@ -9,5 +9,26 @@ package br.unigran.entities;
  * @author valds
  */
 public class Validade {
+    private String data_fabricacao;
+
+    public String getData_fabricacao() {
+        return data_fabricacao;
+    }
+
+    public void setData_fabricacao(String data_fabricacao) {
+        this.data_fabricacao = data_fabricacao;
+    }
     
+      public void calcularDataFabric(){
+          
+      }
+      public void exibirDataVenc(){
+          
+      }
+      public void alterar(){
+          
+      }
+      public void listar(){
+          
+      }
 }
